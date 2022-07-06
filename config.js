6e13eba32a4627d4ca07cf774a5114f673554d5b
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-07-07 00:34:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by Gempy'
+global.wm = '𝙕𝙖𝙡𝙡𝙭𝘽𝙤𝙩𝙯'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
