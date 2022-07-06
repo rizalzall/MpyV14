b34abb179eb81d2146f0261d8a0a5cf36a5ee411
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-07-07 00:31:11 +0700

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -149,5 +149,13 @@ let hiaine = hiain[Math.floor(Math.random() * hiain.length)]
 
 }
 
+
+// salam
+    let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم)/i
+    let isSalam = reg.exec(m.text)
+    if (isSalam && !m.fromMe) {
+        m.reply(`وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n_wa\'alaikumussalam wr.wb._`)
+    }
+
 handler.limit = true
 module.exports = handler
